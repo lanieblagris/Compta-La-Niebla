a4327f6d10c051a082c85031af062fd06c8a60c4
lanieblagris <lanieblagris@gmail.com>
2026-04-03 00:37:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ else:
             else:
                 st.info("Aucune activité cette semaine.")
         else:
-            st.info("Au boulot feneant!")
+            st.info("Au boulot feneant !")
     except Exception as e:
         st.info("En attente de données...")
     st.markdown("---")
